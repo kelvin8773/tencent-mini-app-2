166050c898d94975c3792d7e9e87fdd694ac9d91
Kelvin Liang <kelvin9877@gmail.com>
2026-05-02 17:20:42 +0800

    [KL]: project initialization

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+declare const wx: WechatMiniprogram.Wx;
+declare const App: WechatMiniprogram.App;
+declare const Page: WechatMiniprogram.Page;
+declare const Component: WechatMiniprogram.Component;
+declare const getApp: WechatMiniprogram.GetApp;
+declare const getCurrentPages: WechatMiniprogram.GetCurrentPages;
